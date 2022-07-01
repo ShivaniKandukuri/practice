33e2080e568410a44b4df8d870043f3af52f039d
shivanikandukuri <shivani.kandukuri.@prasthana.com>
2022-07-01 11:55:37 +0530

    d

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,3 @@
 class PagesController < ApplicationController
-  def index
-  #  @v=current_model.articles
-  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to:'pages#index'
+  root to:'pages#home'
   devise_for :models
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
